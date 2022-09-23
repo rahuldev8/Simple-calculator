2ca1810227dc2c44698e921e5e46c2a54552c977
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-23 19:33:27 +0530

    react base

---
+++ Brains/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+function App()
+{
+    return(
+        <div className="App">
+            <h1>Test E-commerce Store</h1>
+        </div>
+    );
+}
+
+export default App;
